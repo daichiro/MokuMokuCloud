f9c81ccbc8871099692bd9d2a9da919a4a6aa79f
mokumoku <da1lawmoku2@gmail.com>
2016-05-31 01:05:37 +0900

    公開版完成
    - og:image 修正
    - Qiita のリンク追加
    - 起動スクリプトを Linux 用に修正

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 service nginx start
-swift build --configuration release
+swift build --configuration release -Xcc -fblocks -Xlinker -rpath -Xlinker .build/release
 /var/www/html/MokuMokuCloud/.build/release/MokuMokuCloud
