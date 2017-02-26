ce462cb2a56aa925e31e404f58be8e74afa09df6
mokumoku <da1lawmoku2@gmail.com>
2017-02-27 01:34:03 +0900

    実績追加、くもゐ画像修正

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo su -
 service nginx start
 swift build --configuration release -Xcc -fblocks -Xlinker -rpath -Xlinker .build/release
 /var/www/html/MokuMokuCloud/.build/release/MokuMokuCloud
